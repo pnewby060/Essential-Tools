f6f2af26079fe6a8a5fc5ae74e1f5d734400a66c
Philip Newby <pnewby060@gmail.com>
2017-04-20 23:47:31 +0100

    Updated the subtitle to show Android device info

--- app/src/main/java/uk/co/acleaninstall/essentialtools/activity/MainActivity.java
+++ app/src/main/java/uk/co/acleaninstall/essentialtools/activity/MainActivity.java
@@ -9,6 +9,8 @@ import android.telephony.PhoneStateListener;
 import at.amartinz.execution.ShellManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.jrummyapps.android.shell.CommandResult;
+import com.jrummyapps.android.shell.Shell;
 import uk.co.acleaninstall.essentialtools.MyApp;
 import uk.co.acleaninstall.essentialtools.R;
 import uk.co.acleaninstall.essentialtools.adapter.MyPagerAdapter;
@@ -28,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.viewpager)
     ViewPager mViewPager;
+
+    public static final String Doof
+        = "echo \"Android $(cat /system/build.prop | grep build.version.release | sed s/.*=//) ($(cat /system/build.prop | grep ro.build.host | sed s/.*=//) $(cat /system/build.prop | grep ro.modversion | sed s/.*=//))\\n$(cat /system/build.prop | grep ro.product.model | sed s/.*=//) ($(cat /system/build.prop | grep ro.product.device | sed s/#.*// | sed s/.*=// | tr -d \\n))\"";
 
 
     @Override
@@ -65,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
 
         // push out toolbar down from no action bar theme value
         mToolbar.setPadding(0, MiscTools.getStatusBarHeight(), 0, 0);
+
+        CommandResult result = Shell.SU.run(Doof);
+
+        mToolbar.setSubtitle(result.isSuccessful() ? result.getStdout() : result.getStderr());
     }
 
 
